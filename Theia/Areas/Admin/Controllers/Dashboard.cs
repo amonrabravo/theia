a995abdc96318859c31ddfbb3d712248748bbe6a
amonrabravo <onay.yalciner@hotmail.com>
2021-01-22 22:21:50 +0300

    areas admin

---
+++ Theia/Areas/Admin/Controllers/Dashboard.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Theia.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class Dashboard : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
