a3e62d9bf870919cb8347d9ac235d1e49eb6250a
amonrabravo <onay.yalciner@hotmail.com>
2021-01-25 20:16:01 +0300

    edit

---
+++ Theia/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Theia.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Authorize(Roles = "ProductAdministrators")]
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
